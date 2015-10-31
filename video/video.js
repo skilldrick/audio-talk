f5aca3f8b78aa0b1503cf3122bec6cd08875f56f
Nick Morgan <skilldrick@gmail.com>
2015-10-30 21:18:12 -0700

    Fix video

--- video/video.js
+++ video/video.js
@@ -9,12 +9,34 @@
   function setupVideo() {
     $container = $('.container');
 
-    var standupcat = new Image();
-    var standupcatsrc = "cat.gif";
-    standupcat.src = standupcatsrc;
+    var srcs = [
+      'standupcat.gif',
+      'smokecat.gif',
+      'pawcat.gif',
+      'breadcat.gif',
+      'wtfcat.gif',
+      'pouncecat.gif',
+      'omgcat.gif',
+      'pianocat.gif',
+      'keyboardcat.gif',
+      'maru.gif',
+    ];
+
+    srcs.forEach(function (src) {
+      $('.hidden').append($('<img>').prop('src', src));
+    });
+
+    function randomEl(arr) {
+      var randomIndex = Math.floor(Math.random() * arr.length);
+      return arr[randomIndex];
+    }
+
+    function randomSrc() {
+      return randomEl(srcs);
+    }
 
     function newCat(pos) {
-      var $cat = $('<img>').addClass('standupcat positionable small').prop('src', standupcatsrc).show();
+      var $cat = $('<img>').addClass('gif positionable small').prop('src', randomSrc()).show();
       return position($cat.appendTo('.container'), pos);
     }
 
@@ -34,59 +56,58 @@
       return $el;
     }
 
+    function makeBig($imgs) {
+      $imgs.each(function () {
+        var $img = $(this);
+        $img.css('height', '40%');
+        $img.filter('.middle').css('left', $container.width() / 2 - $img.width() / 2);
+      });
+    }
+
+    function setContainerHeight() {
+      var height = $(window).height() * 0.9;
+      var width = height * 1.5;
+      $container.height(height).width(width);
+    }
+
+    function restartGifs($els) {
+      $els.each(function () {
+        $(this).prop('src', randomSrc());
+      });
+      return $els;
+    }
+
     $(document).on('beat', function (e, number) {
       console.log('Beat', number);
-
-      if (number % 4 == 0) {
-        $('.standupcat').prop('src', standupcat.src).show();
-        $('body').css('background-color', '#f00');
-      }
-      if (number % 4 == 2) {
-        $('.standupcat').removeAttr('src').hide();
-        $('body').css('background-color', '#0f0');
-      }
 
       if (number == 0) {
         newCat({ middle: true });
       }
+
       if (number == 16) {
         newCat({ left: true });
         newCat({ right: true });
       }
+
       if (number == 32) {
         newCat({ left: true, bottom: true });
         newCat({ right: true, bottom: true });
         newCat({ middle: true, bottom: true });
       }
-      if (number == 48) {
-        position(newCat({}).removeClass('small').css('height', '100%'), { middle: true });
+
+      if (number % 4 == 0) {
+        $('.gif').show();
+        $('body').css('background-color', '#f00');
       }
-      if (number == 64) {
-        $('.standupcat.small').hide();
+      if (number % 4 == 2) {
+        restartGifs($('.gif')).hide();
+        $('body').css('background-color', '#0f0');
       }
 
-      makeBig($('.standupcat.small'));
       setContainerHeight();
+      makeBig($('.gif.small'));
     });
 
     setContainerHeight();
   }
-
-  function makeBig($img) {
-    $img.css('height', '40%');
-    $img.filter('.middle').css('left', $container.width() / 2 - $img.width() / 2);
-  }
-
-  function setContainerHeight() {
-    var height = $(window).height() * 0.9;
-    var width = height * 1.5;
-    $container.height(height).width(width);
-  }
 })();
-/*
-http://25.media.tumblr.com/tumblr_me4to17Fi51rd2l87o1_500.gif;
-http://25.media.tumblr.com/tumblr_m6lz2mqSln1raq43io1_500.gif
-http://25.media.tumblr.com/tumblr_m6qc6t2lze1r4u9wdo1_500.gif
-http://cdn.cinemagr.am/cine_1/51727493.gif
-http://i.imgur.com/RssgfnU.gif
-*/
